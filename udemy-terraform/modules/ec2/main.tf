4be2a95dd29016051b9c2eeb4630856d204ee696
EC2 Default User <ec2-user@ip-172-31-20-114.ap-northeast-1.compute.internal>
2023-05-18 08:14:09 +0000

    finish

--- udemy-terraform/modules/ec2/main.tf
+++ udemy-terraform/modules/ec2/main.tf
@@ -2,7 +2,9 @@ resource "aws_instance" "this" {
   ami           = "ami-0d979355d03fa2522"
   instance_type = "t2.micro"
 
+  subnet_id = var.subnet_id
   vpc_security_group_ids = [aws_security_group.this.id]
+  associate_public_ip_address = true
   
   user_data = file("${path.module}/user_data.sh")
 
@@ -18,7 +20,13 @@ resource "random_id" "this" {
   byte_length = 8
 }
 
+# dataを定義することで、実際のリソースから検索してくれる
+data "aws_subnet" "this" {
+  id = var.subnet_id
+}
+
 resource "aws_security_group" "this" {
+  vpc_id = data.aws_subnet.this.vpc_id
   name = "udemy-terraform-ec2-sg-${random_id.this.hex}"
 }
 
@@ -31,15 +39,6 @@ count = var.allow_ssh ? 1 : 0
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.this.id
 
-}
-
-resource "aws_security_group_rule" "http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.this.id
 }
 
 resource "aws_security_group_rule" "egress" {
